915ec57df20bc748bd23348e7165d3245d773fd9
Niklas Pelli <niklaspelli22@gmail.com>
2023-09-29 14:21:44 +0200

    test

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-function myFunction() {
-    alert("Tack för ditt mail! Vi kommer snart att kontakta dig.");
+ function myFunction() {
+
+    var result = confirm('Är du säker?');
+    if (result == false) {
+        event.preventDefault(balalalala);
+    }
+    }
+  /*   alert("Tack för ditt mail! Vi kommer snart att kontakta dig.");
     location.replace(location.href);
-  }
+  }  */
+
+/* let popup = document.getElementById("popup");
+
+function openPopup(){
+    popup.classList.add("open-popup");
+}
+function closePopup(){
+    popup.classList.remove("open-popup");
+} */
