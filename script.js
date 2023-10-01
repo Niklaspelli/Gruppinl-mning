5569ce91b6313a37c3383b9201b5a07da8176910
Niklas Pelli <niklaspelli22@gmail.com>
2023-10-01 21:51:07 +0200

    småfix

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
- function myFunction() {
+  function myFunction() {
+alert("Tack för ditt mail! Vi kommer snart att kontakta dig."); 
+    location.replace(location.href);
+  }
 
-    var result = confirm('Är du säker?');
+
+
+
+    /* var result = confirm('Är du säker?');
     if (result == false) {
         event.preventDefault(balalalala);
-    }
-    }
-  /*   alert("Tack för ditt mail! Vi kommer snart att kontakta dig.");
-    location.replace(location.href);
-  }  */
+    } */
+    
+/*     */
+  
 
 /* let popup = document.getElementById("popup");
 
