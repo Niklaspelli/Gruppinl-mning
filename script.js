910f1cbc873d3f2a6e59fdd65e5729029119d84a
Niklas Pelli <niklaspelli22@gmail.com>
2023-10-02 14:57:42 +0200

    Fixat require på mejlen

--- script.js
+++ script.js
@@ -1,24 +1,7 @@
-/*   function myFunction() {
+ function myFunction() {
 alert("Tack för ditt mail! Vi kommer snart att kontakta dig."); 
     location.replace(location.href);
-  } */
+  } 
 
-
-
-
-    /* var result = confirm('Är du säker?');
-    if (result == false) {
-        event.preventDefault(balalalala);
-    } */
-    
-/*     */
-  
-
-let popup = document.getElementById("exampleModalLabel");
-
-function openPopup(){
-    popup.classList.add("open-popup");
-}
-function closePopup(){
-    popup.classList.remove("open-popup");
-}
+ /*  var myModal = new bootstrap.Modal(document.getElementById('myModal'), options)
+  myModal.show() */
