6f6a478ef19dbe8c24c2406bede801c503c5957a
Simon Hällström <143492796+hallstrom91@users.noreply.github.com>
2023-10-04 16:46:16 +0200

    updates buttons & js

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
- function myFunction() {
-alert("Tack för ditt mail! Vi kommer snart att kontakta dig."); 
-    location.replace(location.href);
-  } 
+"strict";
 
- /*  var myModal = new bootstrap.Modal(document.getElementById('myModal'), options)
+function myOrder() {
+  alert("Tack för din order. Ett bekräftelsemail skickas till dig omgående.");
+  location.replace(location.href);
+}
+function myMessage() {
+  alert("Ett annat meddelanden.");
+  location.replace(location.href);
+}
+function NoNameJohan() {
+  alert("Ett annat meddelanden igen.");
+  location.replace(location.href);
+}
+
+// Order-Calc Below
+
+/*  var myModal = new bootstrap.Modal(document.getElementById('myModal'), options)
   myModal.show() */
