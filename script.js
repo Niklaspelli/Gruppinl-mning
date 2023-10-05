904d52a7cc6181f9602cd074344d70c549b3c1aa
hallstrom91 <143492796+hallstrom91@users.noreply.github.com>
2023-10-05 11:50:23 +0200

    update

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 "strict";
-
+//Order meddelanden om man trycker på "Beställ" knappen + uppdaterar sidan.
 function myOrder() {
   alert("Tack för din order. Ett bekräftelsemail skickas till dig omgående.");
   location.replace(location.href);
 }
+//Meddelanden om man trycker på "Skicka" knappen + uppdaterar sidan.
 function myMessage() {
   alert("Tack för ditt mail. Vi svarar inom 24tim alla vardagar.");
   location.replace(location.href);
 }
+//Meddelanden om man trycker på "Skicka in" knappen + uppdaterar sidan.
 function mySend() {
   alert("Tack för din ansökan. Vi kontaktar dig inom en snar framtid.");
   location.replace(location.href);
 }
-
-// Order-Calc Below
-
-/*  var myModal = new bootstrap.Modal(document.getElementById('myModal'), options)
-  myModal.show() */
